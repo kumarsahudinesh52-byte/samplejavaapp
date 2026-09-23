c7c21dcd36378f8301359575d493f511f8dd8e18
Dinesh Kumar Sahu <kumarsahudinesh52@gmail.com>
2026-09-23 18:06:57 +0530

    Update PropertyHelper.java

--- src/main/java/com/devopsdemo/utilities/PropertyHelper.java
+++ src/main/java/com/devopsdemo/utilities/PropertyHelper.java
@@ -3,7 +3,7 @@ package com.devopsdemo.utilities;
 import java.util.HashMap;
 import java.util.Properties;
 
-/**
+/***
  * Helper Class to load Properties from a property file to be passed to caller for execution.
  * Multiple properties can be loaded.
  * Note that if the same property is specified multiple times in a single file, there is no guaranteed "Winner".
